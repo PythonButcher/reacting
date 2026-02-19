889569860d083e809c36830f3e42151b6d3ed234
Jon Cochran <46171023+PythonButcher@users.noreply.github.com>
2026-02-19 00:11:48 -0500

    clock
    added clock

--- src/app/AppLayout.jsx
+++ src/app/AppLayout.jsx
@@ -1,19 +1,18 @@
 // AppLayout is the permanent shell of the application
 // No experiments go here. Only layout and navigation.
 
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import MenuBar from "../components/menu/MenuBar";
 import "./App.css";
 
 export default function AppLayout() {
   return (
     <div>
-      <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
-        <Link to="/">Home</Link>{" "}
-        <Link to="/experiments">Experiments</Link>{" "}
-        <Link to="/articles">Articles</Link>
-      </nav>
+      {/* The Shelf is now hung on the wall */}
+      <MenuBar />
 
       <main style={{ padding: "1rem" }}>
+        {/* The Magic Window where pages swap out */}
         <Outlet />
       </main>
     </div>
